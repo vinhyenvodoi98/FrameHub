811100c9f9e0b2c4c582d286e904e79cba9baa54
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2024-06-25 01:03:18 +0900

    update connect wallet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
 'use client'
+import { wagmiConfig } from '@/config/wagmi';
+import { Address, Avatar, Badge, Identity, Name } from '@coinbase/onchainkit/identity';
 import { ConnectAccount } from '@coinbase/onchainkit/wallet';
 import Link from 'next/link';
+import { useAccount } from 'wagmi';
+import { disconnect } from 'wagmi/actions';
 
 export default function Header() {
+  const { address } = useAccount()
+
   return (
     <div className='sticky top-0 z-50 bg-base-100'>
       <div className='flex items-center justify-between px-6 h-20'>
@@ -13,7 +19,26 @@ export default function Header() {
             </Link>
           </div>
           <div className="navbar-end flex gap-4">
-            <ConnectAccount />
+            <div className="flex">
+              {
+                address === undefined ?
+                  <ConnectAccount /> :
+                  <div className="flex bg-white rounded-lg items-center justify-center">
+                    <button type="button" onClick={() => disconnect(wagmiConfig)}>
+                      <Identity
+                        className="bg-transparent"
+                        schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
+                        address={address}
+                      >
+                        <Avatar address={address} />
+                        <Name address={address}>
+                          <Badge />
+                        </Name>
+                      </Identity>
+                    </button>
+                  </div>
+              }
+            </div>
           </div>
         </div>
       </div>
